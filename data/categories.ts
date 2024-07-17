4212fcc18ad5a0e730f5d2ed83a67c474dbbcdcc
Nipuna Theekshana <124187309+NipunaTheekshanaDeMel@users.noreply.github.com>
2024-07-18 00:41:10 +0530

    add shorts file

---
+++ data/categories.ts
@@ -0,0 +1,33 @@
+const destinationCategories = [
+    {
+        title: "",
+        iconName: "compass" ,
+        color: "white",
+    },
+    {
+        title: "All",
+       
+    },
+    {
+        title: "Music",
+        
+    },
+    {
+        title: "Popular",
+        
+    },
+    {
+        title: "Gaming",
+        
+    },
+    {
+        title: "React",
+        
+    },
+    {
+        title: "html",
+        
+    },
+];
+
+export default destinationCategories;
